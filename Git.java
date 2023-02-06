d2a1c0a1bd69283d6def2e155237270176bc0968
Luna <Luna@qq.com>
2023-02-06 13:03:55 +0800

    hot-fix commit!

--- Git.java
+++ Git.java
@@ -2,6 +2,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello Github");
-        System.out.println("hello java");
+        System.out.println("hello java 456");
     }
 }
